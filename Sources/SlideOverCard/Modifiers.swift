ca1151cc92644bdc73fbc5f17ad1586697231419
Elshir <Alxer115@Gmail.com>
2022-09-02 09:37:49 +0800

    Update Modifiers.swift
    Support custom corner radius.

--- Sources/SlideOverCard/Modifiers.swift
+++ Sources/SlideOverCard/Modifiers.swift
@@ -8,21 +8,22 @@
 import SwiftUI
 
 extension View {
-    public func slideOverCard<Content: View>(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, options: SOCOptions = [], backgroundColor: Color = Color(.systemGray6), @ViewBuilder content: @escaping () -> Content) -> some View {
+    public func slideOverCard<Content: View>(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, options: SOCOptions = [], backgroundColor: Color = Color(.systemGray6), customCornerRadius: CGFloat = 38.5, @ViewBuilder content: @escaping () -> Content) -> some View {
         return ZStack {
             self
             SlideOverCard(isPresented: isPresented,
                           onDismiss: onDismiss,
                           options: options,
-                          backgroundColor: backgroundColor) {
+                          backgroundColor: backgroundColor
+                          customCornerRadius: customCornerRadius) {
                 content()
             }
         }
     }
     
-    public func slideOverCard<Item: Identifiable, Content: View>(item: Binding<Item?>, onDismiss: (() -> Void)? = nil, options: SOCOptions = [], backgroundColor: Color = Color(.systemGray6), @ViewBuilder content: @escaping (Item) -> Content) -> some View {
+    public func slideOverCard<Item: Identifiable, Content: View>(item: Binding<Item?>, onDismiss: (() -> Void)? = nil, options: SOCOptions = [], backgroundColor: Color = Color(.systemGray6), customCornerRadius: CGFloat = 38.5,@ViewBuilder content: @escaping (Item) -> Content) -> some View {
         let binding = Binding(get: { item.wrappedValue != nil }, set: { if !$0 { item.wrappedValue = nil } })
-        return self.slideOverCard(isPresented: binding, onDismiss: onDismiss, options: options, backgroundColor: backgroundColor, content: {
+        return self.slideOverCard(isPresented: binding, onDismiss: onDismiss, options: options, backgroundColor: backgroundColor, customCornerRadius: customCornerRadius, content: {
             if let item = item.wrappedValue {
                 content(item)
             }
